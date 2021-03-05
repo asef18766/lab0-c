5f88d23bbf47bcfbf76269835ac1df8cbd811298
ubuntu20.04 <asef18766@gmail.com>
2021-03-05 01:50:27 -0800

    Fix time measurement

--- dudect/cpucycles.h
+++ dudect/cpucycles.h
@@ -1,12 +1,9 @@
 #include <stdint.h>
-// http://www.intel.com/content/www/us/en/embedded/training/ia-32-ia-64-benchmark-code-execution-paper.html
+#include <time.h>
+
 static inline int64_t cpucycles(void)
 {
-#if defined(__i386__) || defined(__x86_64__)
-    unsigned int hi, lo;
-    __asm__ volatile("rdtsc\n\t" : "=a"(lo), "=d"(hi));
-    return ((int64_t) lo) | (((int64_t) hi) << 32);
-#else
-#error Unsupported Architecture
-#endif
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return t.tv_nsec;
 }
